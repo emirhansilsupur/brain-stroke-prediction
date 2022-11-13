2b9d3deea676d72029779b6ad41d65e3c6e3003a
Emirhan Silsüpür <sr.emirhn@gmail.com>
2022-11-13 19:41:58 +0300

    add slider

--- app.py
+++ app.py
@@ -21,19 +21,28 @@ if page =="Prediction":
     st.markdown("<h1 style='text-align:center;'>Brain Stroke Prediction</h1>",unsafe_allow_html=True)
     st.markdown("<h2 style='text-align:center;'>Personal Information</h2>",unsafe_allow_html=True)
     gender_ = st.radio("Gender",["Female","Male"])
-    age_ = st.number_input("Age",step=1.,format="%.2f")
+    age_ = st.slider("Age",0,100)
     work_ = st.radio("Work Type",["Children","Private","Self-Employed","Government Job"])
     ever_married_ = st.radio("Did you ever get married?",["Yes","No"])
     residence_ = st.radio("What is your residence type?",["Urban","Rural"])
 
     st.markdown("<h2 style='text-align:center;'>Health Information</h2>",unsafe_allow_html=True)
     smoking_ = st.radio("What is your smoking type?",["Smoker","Formerly smoker","Never smoked","Unknown"])
-    bmi_ = st.number_input("BMI",step=1.,format="%.2f")
-    glucose_level_ = st.number_input("Glucose Level(eAG)",step=1.,format="%.2f")
+    if st.checkbox("Dont Know BMI? Use height and weight"):
+        height = st.slider("Enter User's Height in cm",value=100.,step=1.,format="%.2f")
+        weight = st.slider("Enter User's Weight in kgs",value=100.,step=1.,format="%.2f")
+        bmi_ = weight / (height/100)**2
+        st.write("BMI of user is {:.2f} and will be autoupdated".format(bmi_))
+    else:
+        bmi_ = st.slider("Enter User's BMI",min_value=10.,max_value=50.,step=0.01,format="%.2f")
+    #bmi_ = st.number_input("BMI",step=1.,format="%.2f")
+    glucose_level_ = st.slider("Glucose Level(eAG)",min_value=0.,max_value=300.,step=1.,format="%.2f")
     heart_ = st.radio("Do you have heart disease?",["Yes","No"])
     hypertension_ = st.radio("Do you have hypertension?",["Yes","No"])
     button=st.button("Click me")
     
+    
+
     cols=["gender","age","hypertension","heart_disease","ever_married","residence_type","bmi","avg_glucose_level","work_type", "smoking_status"]
 
     row = np.array([gender_,age_,hypertension_,heart_,ever_married_,residence_,bmi_,glucose_level_,work_,smoking_])
